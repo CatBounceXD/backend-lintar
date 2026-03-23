46de2c418eea16d6e4aea67e6cd2f328af11ab0a
CatBounceXD <steventrue737@gmail.com>
2026-03-23 22:38:12 +0700

    chore: inisialisasi project backend

---
+++ README.md
@@ -0,0 +1,10 @@
+# Project UTS Backend Programming - Kelompo
+Proyek ini adalah RESTful API yang dibangun menggunakan Node.js, Express.js, dan MongoDB (Mongoose), tugas Ujian Tengah Semester (UTS), Backend Programming.
+
+## Code yang Digunakan
+* **Runtime:** Node.js
+* **Framework:** Express.js
+* **Database:** MongoDB Atlas (Cloud)
+* **ODM:** Mongoose
+* **Environment:** dotenv
+
--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+require('dotenv').config(); 
 const express = require('express');
-const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
 
+const app = express();
+const PORT = 3000;
+
+// Middleware agar API bisa menerima data dalam JSON
 app.use(express.json());
 
+// sambungan server ke MongoDB Atlas dengan URL dr .env
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Berhasil terhubung ke MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.error('Gagal terhubung ke MongoDB:', error.message);
+  });
+
+// Endpoint dasar untuk mengetes server
 app.get('/', (req, res) => {
-  res.send('Server REST API Website Kampus berhasil menyala!');
+    res.send('Server REST API Website Kampus berhasil menyala!');
 });
 
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+// Perintah untuk menyalakan server
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
 });

02e99c9e67691af888d54f42fe9f1e522f5f04d8
unknown <syafiqaaida9@gmail.com>
2026-04-13 11:04:41 +0700

    menambahkan courses

---
+++ src/api/components/courses/fika-service.js
@@ -0,0 +1,22 @@
+const CourseRepository = require('./courses.repository');
+
+class CourseService {
+  async getAllCourses() {
+    return await CourseRepository.findAll();
+  }
+
+  async createCourse(data) {
+    // Anda bisa menambahkan validasi bisnis tambahan di sini
+    return await CourseRepository.create(data);
+  }
+
+  async updateCourse(id, data) {
+    return await CourseRepository.update(id, data);
+  }
+
+  async deleteCourse(id) {
+    return await CourseRepository.delete(id);
+  }
+}
+
+module.exports = new CourseService();

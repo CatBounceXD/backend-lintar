3f223ba69ae734fa943b3c209a29578ce69c264e
keylparker <xiaomikaylani2007@gmail.com>
2026-04-14 11:35:13 +0700

    memasukkan code

---
+++ src/api/components/schedules/schedule-route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const ScheduleController = require('../controllers/schedule.controller');
+
+router.get('/', ScheduleController.getAll);
+router.post('/', ScheduleController.create);
+router.get('/:id', ScheduleController.getById);
+router.put('/:id', ScheduleController.update);
+router.delete('/:id', ScheduleController.delete);
+
+module.exports = router;

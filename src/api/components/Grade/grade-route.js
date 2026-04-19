66b3af85f41abe43ff1cc7f84acc635b479aeb91
unknown <sekarputri5795@gmail.com>
2026-04-19 14:42:33 +0700

    menambahkan stipen

--- src/api/components/Grade/grade-route.js
+++ src/api/components/Grade/grade-route.js
@@ -1,8 +1,6 @@
-const express = require('express');
-const router = express.Router();
 const gradeController = require('./grade-controller');
 
-router.post('/', gradeController.create);
-router.get('/student/:studentId', gradeController.getHistory);
-
-module.exports = router; // 
+module.exports = (app) => {
+  app.post('/grades', gradeController.create);
+  app.get('/grades/student/:studentId', gradeController.getHistory);
+};
